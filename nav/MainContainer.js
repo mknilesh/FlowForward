1cc9f93b0f5fb303d29914c99d1aefc12c504d87
Jasleen <42920356+JasleenSidhu20@users.noreply.github.com>
2023-11-04 19:52:22 -0400

    firebase set up

--- nav/MainContainer.js
+++ nav/MainContainer.js
@@ -6,11 +6,13 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Dashboard from './screens/Dashboard';
 import WorkoutsScreen from './screens/WorkoutsScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import Login from './screens/Login';
 
 //Screen names
 const dashboardName = "Dashboard";
 const workoutsName = "Workouts";
 const settingsName = "Settings";
+const loginName = "Login";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,17 +20,18 @@ function MainContainer() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName={dashboardName}
+        initialRouteName={loginName}
         screenOptions={{
             tabBarActiveTintColor: '#ff80a0',
             tabBarInactiveTintColor: 'grey',
             tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
             tabBarStyle: { padding: 10, height: 70}
         }}>
-
+        <Tab.Screen title={"Login"} name={loginName} component={Login} />
         <Tab.Screen title={"Dashboard"} name={dashboardName} component={Dashboard}/>
         <Tab.Screen title={"Workouts"} name={workoutsName} component={WorkoutsScreen} />
         <Tab.Screen title={"Settings"} name={settingsName} component={SettingsScreen} />
+        
 
       </Tab.Navigator>
     </NavigationContainer>
